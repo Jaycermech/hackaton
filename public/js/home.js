b5eff604a4c366d600b222d5f7374c2f011c7868
Jovan Tang <2204714F@student.tp.edu.sg>
2023-11-06 14:25:08 +0800

    piechart_added

--- public/js/home.js
+++ public/js/home.js
@@ -30,6 +30,7 @@ function viewResources() {
     }
 
     console.log(categories);
+    displayPie(categories);
 
     for (let [key, value] of categories) {
       html +=
@@ -38,6 +39,62 @@ function viewResources() {
     document.getElementById("tableContent").innerHTML = html;
   };
   request.send();
+}
+
+function displayPie(data) {
+  console.log(data);
+
+  // for (let [key, value] of categories) {
+  //   console.log(key + " = " + value);
+  //   for (var j = 0; j < response.length; j++) {
+  //     if (response[j].description === key) {
+  //       console.log(response[j].description);
+  //       console.log(response[j].amount);
+  //       value += parseFloat(response[j].amount);
+  //       categories.set(key, parseFloat(value.toFixed(2))); // Convert the value to a float with two decimal points
+  //       console.log("am i being run");
+  //     }
+  //   }
+  // }
+
+  // Your existing viewResources function logic here
+  // Obtain the necessary data for the pie chart
+  // For instance, let's say you have an array of expenses
+  // const expensesData = [
+  //   { category: "Utilities", amount: 200 },
+  //   { category: "Groceries", amount: 350 },
+  //   // ... other expense categories and amounts
+  // ];
+
+  const expensesData = Array.from(data, ([category, amount]) => ({
+    category,
+    amount,
+  }));
+
+  // Prepare data for the chart
+  const categories = expensesData.map((expense) => expense.category);
+  const amounts = expensesData.map((expense) => expense.amount);
+
+  // Render the pie chart
+  const ctx = document.getElementById("expensesChart").getContext("2d");
+  const expensesChart = new Chart(ctx, {
+    type: "pie",
+    data: {
+      labels: categories,
+      datasets: [
+        {
+          data: amounts,
+          backgroundColor: [
+            "rgba(255, 99, 132, 0.7)", // Color for Utilities
+            "rgba(54, 162, 235, 0.7)", // Color for Groceries
+            "rgba(54, 162, 135, 0.7)",
+
+            // Add more colors for other categories if needed
+          ],
+        },
+      ],
+    },
+  });
 }
 
 function addResource() {
